7c5114be7112ac47a357a1729bdd4d7d57220a84
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-06-28 13:59:11 +1000

    edit readme and add changes to C# script for user paddle

--- Assets/Scripts/MovePaddle.cs
+++ Assets/Scripts/MovePaddle.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class MovePaddle : MonoBehaviour {
 
-    //public float speed = 10.0F;
-    //void FixedUpdate()
-    //{
-    //    float moveVert = Input.GetAxisRaw("Vertical") * speed;
-    //    float moveHor = Input.GetAxisRaw("Horizontal") * speed;
-    //    GetComponent<Rigidbody2D>().velocity = new Vector2(moveVert, 0);
-    //    GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveHor);
-
-    //}
+    
     public float speed = 50;
 
     void FixedUpdate()
     {
+        // If you want paddle to move horizontally as well:
+        //float vertPress = Input.GetAxisRaw("Vertical");
+        //float horPress = Input.GetAxisRaw("Horizontal");
+
+        //    if (vertPress != 0f) {
+        //        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
+
+        //    }
+        //    else
+        //    {
+        //        GetComponent<Rigidbody2D>().velocity = new Vector2(horPress, 0) * speed;
+        //    }
 
         float vertPress = Input.GetAxisRaw("Vertical");
-        float horPress = Input.GetAxisRaw("Horizontal");
 
-        if ( vertPress != 0f) {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
-            
-        }
-        else
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(horPress, 0) * speed;
-        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
 
-        //float vertPress = Input.GetAxisRaw("Vertical");
-        //Rigidbody2D rigid = GetComponent<Rigidbody2D>();
-
-        
     }
-        // Use this for initialization
+    // Use this for initialization
     void Start () {
 		
 	}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-#### Pong on Unity 
+#### Pong in Unity 
+
+Add this script to your left paddle:
+
+```
+public float speed = 50;
+    void FixedUpdate()
+    {
+        float vertPress = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
+    }
+```
